a4e00f81ed7071c44586e54cfc5caecc90b11fc2
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2024-04-15 12:21:46 +0100

    Refactor webpack configuration for better code organisation
    - Update entry point to use an object syntax for better scalability
    - Modify output filename to use dynamic naming using [name] placeholder
    - Improve readability and maintainability of the configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
@@ -11,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
